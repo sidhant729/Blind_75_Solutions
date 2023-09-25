98b4b8b85dc0274949eeb55b14921d2a0c3dae42
sidhant729 <sidhant1841.be21@chitkara.edu.in>
2023-09-25 18:59:25 +0530

    Number Of 1 Bits
    Number Of 1 Bits

---
+++ Number_Of_1_Bits.cpp
@@ -0,0 +1,19 @@
+// Problem Link -> https://leetcode.com/problems/number-of-1-bits/description/
+
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) 
+    {
+        int count = 0;
+        while(n > 0)
+        {
+            if(n & 1 != 0) count++;
+            n = n >> 1;
+        }   
+        return count;
+    }
+};
+
+// T.C. -> O(log n)
+// S.C. -> O(1)
